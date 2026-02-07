1140673a82b362d5b74afd0530c95b6ab7f9fff7
Jodie Cloete <cloete.jodie@gmail.com>
2026-02-07 13:41:19 -0700

    Add: Theme button and ThemeContext component added

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,6 +1,13 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext";
+import { useContext } from "react";
+
+// export default function Header() {
+
 export default function Header() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <header className={styles.appHeader}>
       <h1>
@@ -24,6 +31,14 @@ export default function Header() {
         >
           ❤️
         </Link>
+
+        <button
+          onClick={toggleTheme}
+          className={styles.themeToggle}
+          aria-label="Toggle theme"
+        >
+          {theme === "light" ? "🌞" : "🌙"}
+        </button>
       </div>
     </header>
   );
