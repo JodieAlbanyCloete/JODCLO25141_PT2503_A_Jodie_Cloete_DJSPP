0f2ca00a015a68934bc0d4ae27f63986f6354155
Jodie Cloete <cloete.jodie@gmail.com>
2026-02-10 20:35:42 -0700

    Add: SortSelect updated to sort by A-Z and Z-A

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useFavourites } from "../context/FavouritesContext";
+import { PodcastContext } from "../context/PodcastContext";
+
 import FavouritePodcast from "../components/Podcasts/FavouritePodcasts";
 import SortSelect from "../components/Filters/SortSelect";
 
 export default function FavouritesPage() {
   const { favourites } = useFavourites();
+  const { sortKey } = useContext(PodcastContext);
 
   if (!favourites.length) {
     return <p style={{ padding: "1rem" }}>No favourites yet 🎧</p>;
   }
 
+  const data = [...favourites];
+
+  switch (sortKey) {
+    case "title-asc":
+      data.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+
+    case "title-desc":
+      data.sort((a, b) => b.title.localeCompare(a.title));
+      break;
+
+    case "date-asc":
+      data.sort((a, b) => a.episode - b.episode);
+      break;
+
+    case "date-desc":
+      data.sort((a, b) => b.episode - a.episode);
+      break;
+
+    case "default":
+    default:
+      break;
+  }
+
   return (
     <div style={{ padding: "1rem" }}>
       <h2>My Favourites</h2>
-      <h3> Your saved episodes from all shows</h3>
+      <h3>Your saved episodes from all shows</h3>
 
       <SortSelect />
 
-      {favourites.map((ep) => (
+      {data.map((ep) => (
         <FavouritePodcast
-          key={ep.id}
+          key={`${ep.id}-${ep.episode}`}
           episode={ep}
           season={{ title: ep.seasonTitle }}
           podcast={{ title: ep.podcastTitle, image: ep.podcastImage }}
